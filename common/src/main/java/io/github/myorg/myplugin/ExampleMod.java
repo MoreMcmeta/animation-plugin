d85bed9d42564db11765ff2daac0e4d219174939
soir20 <71418127+soir20@users.noreply.github.com>
2022-08-29 23:42:33 -0400

    Update mod ID in Gradle props + annotation

--- common/src/main/java/io/github/myorg/myplugin/ExampleMod.java
+++ common/src/main/java/io/github/myorg/myplugin/ExampleMod.java
@@ -1,7 +1,7 @@
 package io.github.myorg.myplugin;
 
 public class ExampleMod {
-    public static final String MOD_ID = "myplugin";
+    public static final String MOD_ID = "moremcmeta_animation_plugin";
 
     public void start() {
 
